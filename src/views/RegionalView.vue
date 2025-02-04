19f6f973cec6b6ea78f6fd6441c52fa038123661
Ben Cooley <bcooley@uvm.edu>
2025-02-04 14:16:17 -0500

    store map view

--- src/views/RegionalView.vue
+++ src/views/RegionalView.vue
@@ -1,12 +1,21 @@
+
+
 <template>
     <div class="about">
       <div class="about-container">
       <h1 class="title" style="margin-top: 50px">Select a region</h1>
-        
+      <ZoningMap 
+          mapboxToken="pk.eyJ1IjoiYmRjb29sZXkiLCJhIjoiY20zM2Nzd212MWl3cTJrcTM5NWNrcjk0byJ9.40rHMMd1TXsvF8zeqxLaBw"
+           />
       </div>
     </div>
   </template>
-  
+
+<script setup>
+import ZoningMap from '@/components/ZoningMap.vue';
+
+</script>
+
   <style>
     .about-container {
       max-width: 900px;
