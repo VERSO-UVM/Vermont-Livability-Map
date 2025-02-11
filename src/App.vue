c175468ac97d4b58da9a0558aea617f9d7722e1b
Ben Cooley <bcooley@uvm.edu>
2025-02-11 15:18:25 -0500

    rm regional highlight from nav

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="45" height="45" />
       <nav id="site-nav">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/regional">Regional Highlights</RouterLink>
+        <!-- <RouterLink to="/regional">Regional Highlights</RouterLink> -->
       </nav>
     
       <section class="hero">
